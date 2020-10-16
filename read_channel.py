69e0c4d070652eab6b3213987514a64152c45806
Eduardo Collado <edu@tecnocratica.net>
2020-10-16 18:42:33 +0200

    commenting functions

--- read_channel.py
+++ read_channel.py
@@ -3,6 +3,9 @@
 import yaml
 
 def generate_channel():
+    '''
+    This function returns a dictionary generated from the yaml file
+    '''
     with open('channel.yaml') as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
         return(data)
--- read_items.py
+++ read_items.py
@@ -3,9 +3,13 @@
 import yaml
 import os
 import glob
-path='items/'
-items=[]
+path='items/' # Directory where I can find the podcast chapters
+items=[] # empty list of dictonaries
 def generate_items():
+    '''
+    This function returns a list of dictionaries generated from the yaml files int
+    the directory defined in path
+    '''
     for filename in glob.glob(os.path.join(path, '*.yaml')):
         with open(os.path.join(filename), 'r') as f: # open in readonly mode
             data = yaml.load(f, Loader=yaml.FullLoader)
